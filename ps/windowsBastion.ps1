18a0567a1c55654ac85db645054e5aa1859cfd50
Caleb Barthlome <c.barthlome@f5.com>
2022-08-04 16:43:12 -0600

    Update windowsBastion.ps1 to resolve install issues with OpenSSH service

--- ps/windowsBastion.ps1
+++ ps/windowsBastion.ps1
@@ -1,13 +1,14 @@
 <powershell>
 # Create new local user and add to administrator group
-$password = "Wind0wsS3rverSecure!" | ConvertTo-SecureString -AsPlainText -Force
+$password = ConvertTo-SecureString "Wind0wsS3rverSecure!" -AsPlainText -Force
 $username = "windowsdemo"
-
 New-LocalUser $username -Password $password -FullName "demo f5"
-
 Add-LocalGroupMember -Group "Administrators" -Member $username
 
 # Install OpenSSH Server
+Set-ItemProperty -Path HKLM:SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU -Name UseWUServer -Value 0
+Restart-Service -Name wuauserv -Force
+Get-WindowsCapability -Online | Where-Object Name -like 'OpenSSH*'
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 
 # Start the sshd service
@@ -16,11 +17,6 @@ Start-Service sshd
 # OPTIONAL but recommended:
 Set-Service -Name sshd -StartupType 'Automatic'
 
-# Confirm the Firewall rule is configured. It should be created automatically by setup. Run the following to verify
-if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
-    Write-Output "Firewall Rule 'OpenSSH-Server-In-TCP' does not exist, creating it..."
-    New-NetFirewallRule -Name 'OpenSSH-Server-In-TCP' -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
-} else {
-    Write-Output "Firewall rule 'OpenSSH-Server-In-TCP' has been created and exists."
-}
+# Configure firewall rule
+New-NetFirewallRule -Name 'OpenSSH-Server-In-TCP' -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
 </powershell>
