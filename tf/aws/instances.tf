a5f4ac2b4346164e67927948f9bc2bce9732d563
Caleb Barthlome <c.barthlome@f5.com>
2022-08-04 15:38:27 -0600

    Create windowsBastion.ps1 script to setup Windows host with necessary programs

--- tf/aws/instances.tf
+++ tf/aws/instances.tf
@@ -43,6 +43,7 @@ resource "aws_instance" "windows_bastion" {
   ami = var.windows_bastion_ami
   instance_type = var.aws_instance_size
   key_name      = aws_key_pair.demo.key_name
+  get_password_data = true
 
   network_interface {
     network_interface_id = aws_network_interface.windows_bastion_public.id
@@ -56,29 +57,22 @@ resource "aws_instance" "windows_bastion" {
 
   tags = {
     Name = "windows_bastion_host"
-  }
-/*
-  user_data = "${file("../bash/ec2Bastion.sh")}"
+  } 
+
+  user_data = "${file("../ps/windowsBastion.ps1")}"
 
   connection {
     type = "ssh"
-    user = "ec2-user"
-    password = ""
+    user = "Administrator"
+    password = "${rsadecrypt(self.password_data,file("${aws_key_pair.demo.key_name}"))}"
     private_key = file(lookup(var.aws_key_pair_file, "private"))
     host = self.public_ip
-  }
+  } 
 
   provisioner "file" {
     source = "~/.ssh/demo_id_rsa"
-    destination = "/home/ec2-user/.ssh/id_rsa"
+    destination = "C:\\Users\\windowsdemo\\.ssh\\id_rsa"
   }
-
-  provisioner "remote-exec" {
-    inline = [
-      "chmod 600 /home/ec2-user/.ssh/id_rsa"
-    ]
-  }
-  */
 }
 
 resource "aws_instance" "webserver-aza" {
