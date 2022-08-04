756ed975c183ff9d7bc077c2488e91be82be7ccd
Caleb Barthlome <c.barthlome@f5.com>
2022-08-04 16:48:07 -0600

    resolve more merge conflicts

--- ps/windowsBastion.ps1
+++ ps/windowsBastion.ps1
@@ -1,6 +1,5 @@
 <powershell>
 # Create new local user and add to administrator group
-<<<<<<< HEAD
 $password = ConvertTo-SecureString "Wind0wsS3rverSecure!" -AsPlainText -Force
 $username = "windowsdemo"
 New-LocalUser $username -Password $password -FullName "demo f5"
@@ -10,16 +9,6 @@ Add-LocalGroupMember -Group "Administrators" -Member $username
 Set-ItemProperty -Path HKLM:SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU -Name UseWUServer -Value 0
 Restart-Service -Name wuauserv -Force
 Get-WindowsCapability -Online | Where-Object Name -like 'OpenSSH*'
-=======
-$password = "Wind0wsS3rverSecure!" | ConvertTo-SecureString -AsPlainText -Force
-$username = "windowsdemo"
-
-New-LocalUser $username -Password $password -FullName "demo f5"
-
-Add-LocalGroupMember -Group "Administrators" -Member $username
-
-# Install OpenSSH Server
->>>>>>> main
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 
 # Start the sshd service
@@ -28,16 +17,6 @@ Start-Service sshd
 # OPTIONAL but recommended:
 Set-Service -Name sshd -StartupType 'Automatic'
 
-<<<<<<< HEAD
 # Configure firewall rule
 New-NetFirewallRule -Name 'OpenSSH-Server-In-TCP' -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
-=======
-# Confirm the Firewall rule is configured. It should be created automatically by setup. Run the following to verify
-if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
-    Write-Output "Firewall Rule 'OpenSSH-Server-In-TCP' does not exist, creating it..."
-    New-NetFirewallRule -Name 'OpenSSH-Server-In-TCP' -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
-} else {
-    Write-Output "Firewall rule 'OpenSSH-Server-In-TCP' has been created and exists."
-}
->>>>>>> main
 </powershell>
