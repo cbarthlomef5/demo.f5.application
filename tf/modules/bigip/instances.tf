7ca27552d7215d81ccca5b2c24ff697a891115ae
Caleb Barthlome <c.barthlome@f5.com>
2022-08-08 10:59:30 -0600

    Correct missing subnet id in bigip_instance setup

--- tf/modules/bigip/instances.tf
+++ tf/modules/bigip/instances.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "bigip_terraform_server" {
   ami = "ami-0cea098ed2ac54925"
   instance_type = "t2.micro"
   key_name      = "demo"
-  subnet_id = ""
+  subnet_id = var.management_subnet
 
   tags = {
     Name = "bigip_terraform_server"
