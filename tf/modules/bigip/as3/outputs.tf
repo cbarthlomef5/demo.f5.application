8ad2acfca8b581f70a5ff9306fb26dc2e7e9f779
Caleb Barthlome <c.barthlome@f5.com>
2022-08-08 21:40:59 -0600

    Remove output files only used for troubleshooting

--- tf/modules/bigip/as3/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "cfe_failover_tag" {
-  value = var.cfe_failover_tag
-}
-
-output "bigip_json_vars" {
-  value = local.bigip_env_json
-}
--- tf/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "main_bigip_objects_json" {
-  value = module.aws_infrastructure.bigip_objects_json
-}
