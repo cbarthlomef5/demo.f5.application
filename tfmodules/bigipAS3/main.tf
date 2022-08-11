a15be8afb4ad5bd911ca57fb028f32b42c1b251a
Caleb Barthlome <c.barthlome@f5.com>
2022-08-11 16:47:52 -0600

    Remove unneeded code

--- tfmodules/bigipAS3/main.tf
+++
@@ -1,6 +0,0 @@
-/*
-resource "local_file" "bigip_as3_declaration_create" {
-  content = data.template_file.as3_declaration_file.rendered
-  filename = "${path.module}/as3/as3_declaration.json"
-}
-*/
