a090016eb9568a000c3839655f8dfb8581928d58
Caleb Barthlome <c.barthlome@f5.com>
2022-08-22 16:30:43 -0600

    Remove execss quotation marks

--- tfmodules/webservers/scripts/bash/bootstrapWebserver.sh
+++ tfmodules/webservers/scripts/bash/bootstrapWebserver.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "Updating and cleaning system"
+# Update and clean system
 yum update -y
 yum upgrade -y
 yum autoremove -y
 
-echo "Install LEMP"
+# Install LEMP
 yum install nginx -y
 amazon-linux-extras install nginx1 -y
 amazon-linux-extras enable php8.0 -y
@@ -13,12 +13,12 @@ yum clean metadata
 yum install php php-cli php-mysqlnd php-pdo php-common php-fpm -y
 yum install php-gd php-mbstring php-xml php-dom php-intl php-simplexml -y
 
-echo "Create necessary files"
-mkdir "/var/www/apps"
-echo "${app_config}" >> /var/www/apps/index.php
-echo "${nginx_config}" >> /etc/nginx/conf.d/apps.conf
+# Create necessary files
+mkdir /var/www/apps
+echo '${app_config}' >> /var/www/apps/index.php
+echo '${nginx_config}' >> /etc/nginx/conf.d/apps.conf
 
-echo "Start LEMP"
+# Start LEMP
 systemctl start nginx.service
 systemctl enable nginx
 systemctl start php-fpm
