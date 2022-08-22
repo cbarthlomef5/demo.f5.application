079609566f466a19983a075eaedec151582a3ec3
cbarthlomef5 <103775647+cbarthlomef5@users.noreply.github.com>
2022-08-22 13:47:52 -0600

    Add files to create dynamic php website for demo app (#13)

--- tfmodules/webservers/scripts/bash/bootstrapWebserver.sh
+++ tfmodules/webservers/scripts/bash/bootstrapWebserver.sh
@@ -5,7 +5,16 @@ yum update -y
 yum upgrade -y
 yum autoremove -y
 
-echo "Install NGINX"
+echo "Install LEMP"
 yum install nginx -y
 amazon-linux-extras install nginx1 -y
+amazon-linux-extras enable php8.0 -y
+yum clean metadata
+yum install php php-cli php-mysqlnd php-pdo php-common php-fpm -y
+yum install php-gd php-mbstring php-xml php-dom php-intl php-simplexml -y
+
+echo "Start LEMP"
 systemctl start nginx.service
+systemctl enable nginx
+systemctl start php-fpm
+systemctl enable php-fpm
